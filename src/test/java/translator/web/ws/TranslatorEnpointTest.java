fbf489d74287e2de995f4e1447f0388016d442a7
Ramon Jose Arellano Bitian <594040@unizar.es>
2015-11-04 14:15:44 +0100

    allDoneIThink

--- src/test/java/translator/web/ws/TranslatorEnpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEnpointTest.java
@@ -42,11 +42,11 @@ public class TranslatorEnpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("I have the fire, I have the force, I have the power to make my evil take its course");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("Tengo el fuego, tengo fuerza, tengo el poder de hacer que mi mal siga su curso"));
 	}	
 }
